7af8cc262605082160c750b55572e0f4738c87ea
denyellow <denyellow@gmail.com>
2020-08-03 10:36:45 +0300

    Решил задачу с первым заглавным символом

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,5 +4,10 @@
  * @returns {string}
  */
 function ucFirst(str) {
-  // ваш код...
+  if (+str !== 0) {
+    str = str[0].toUpperCase() + str.slice(1).toLowerCase();
+  } else {
+    str = '';
+  }
+  return str;
 }
