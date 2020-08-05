442da643e301c30589214c0a2bb0974289b98b0d
denyellow <denyellow@gmail.com>
2020-08-05 18:33:16 +0300

    Доработал функцию до универсальной

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,12 +4,12 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
+  let total = 0;
   for (let key in salaries) {
     if (salaries[key] > 0) {
-      salaries = salaries.John + salaries.Ann + salaries.Pete;
-      return salaries;
-    } else {
-      return 0;
+      console.log(salaries[key]);
+      total += salaries[key];
     }
   }
+  return total;
 }
