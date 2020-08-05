9d77a4d973c99624322a6959042c93db4b7dd23b
denyellow <denyellow@gmail.com>
2020-08-05 18:16:47 +0300

    Написал функцию суммирования зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  for (let key in salaries) {
+    if (salaries[key] > 0) {
+      salaries = salaries.John + salaries.Ann + salaries.Pete;
+      return salaries;
+    } else {
+      return 0;
+    }
+  }
 }
