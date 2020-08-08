e2baa869c2d871a0d8293f96767512754735d39b
denyellow <denyellow@gmail.com>
2020-08-08 16:16:08 +0300

    Трансформировал в массив имен

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -3,5 +3,9 @@
  * @returns {string[]}  объект
  */
 function namify(users) {
-  // ваш код...
+  let arr = [];
+  for (let i = 0; i < users.length; i++) {
+    arr.push(users[i].name);
+  }
+  return arr;
 }
