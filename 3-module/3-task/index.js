9ac243ac94c6fac5b6c2023afadd4befa469558a
denyellow <denyellow@gmail.com>
2020-08-09 15:08:50 +0300

    Функция camelize(str), вариант 2, с join

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -4,12 +4,16 @@
  */
 function camelize(str) {
   let array = str.split('-');
-  let newStr = array[0];
-  if (array.length > 1) {
-    for (i = 1; i < array.length; i++) {
-      let word = array[i];
-      newStr += word[0].toUpperCase() + word.slice(1);
-    }
+  let newArr = [];
+  newArr[0] = array[0];
+
+
+  for (i = 1; i < array.length; i++) {
+    let word = array[i];
+    let newWord = word[0].toUpperCase() + word.slice(1);
+    newArr.push(newWord);
   }
+  let newStr = newArr.join('');
+
   return newStr;
 }
