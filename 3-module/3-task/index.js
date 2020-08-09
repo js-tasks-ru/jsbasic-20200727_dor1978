7a7283437f86b6c7fe449bcd8efc86a7c2e1f774
denyellow <denyellow@gmail.com>
2020-08-09 13:41:14 +0300

    Функция camelize(str), вариант 1, без join

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,13 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  let array = str.split('-');
+  let newStr = array[0];
+  if (array.length > 1) {
+    for (i = 1; i < array.length; i++) {
+      let word = array[i];
+      newStr += word[0].toUpperCase() + word.slice(1);
+    }
+  }
+  return newStr;
 }
