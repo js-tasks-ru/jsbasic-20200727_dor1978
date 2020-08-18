cf0d87f57b74fa06ddf7bb13fcf53be4696b7b6c
denyellow <denyellow@gmail.com>
2020-08-18 14:57:51 +0300

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,27 @@
  * @param {Element} table
  */
 function highlight(table) {
+    for (let i = 0; i < table.rows.length; i++) {
+        let age = table.rows[i].cells[1];
+        let gender = table.rows[i].cells[2];
+        let status = table.rows[i].cells[3];
+
+        if (status.dataset.available === 'true') {
+            table.rows[i].classList.add('available');
+        } else if (status.dataset.available === 'false') {
+            table.rows[i].classList.add('unavailable');
+        } else {
+            table.rows[i].hidden = true;
+        }
+
+        if (gender.innerHTML === 'm') {
+            table.rows[i].classList.add('male');
+        } else {
+            table.rows[i].classList.add('female');
+        }
+
+        if (age.innerHTML < 18) {
+            table.rows[i].style.textDecoration = 'line-through';
+        }
+    }
 }
