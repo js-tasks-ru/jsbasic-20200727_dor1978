e4e75adbf4ba3e0cda8d456fba7dbc1649868a6a
denyellow <denyellow@gmail.com>
2020-08-23 15:22:53 +0300

    Учебный проект: Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  let inner = document.querySelector('.carousel__inner');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let width = inner.offsetWidth;
+
+  let count = 0;
+  arrowLeft.style.display = 'none';
+
+  function translateRight() {
+    inner.style.transform = `translateX(${-(count + 1) * width}px)`;
+
+    count++;
+
+    if (count > 0) {
+      arrowLeft.style.display = '';
+    }
+    if (count === 3) {
+      arrowRight.style.display = 'none';
+    }
+  }
+
+  function translateLeft() {
+    inner.style.transform = `translateX(${-(count - 1) * width}px)`;
+    count--;
+
+    if (count < 3) {
+      arrowRight.style.display = '';
+    }
+    if (count === 0) {
+      arrowLeft.style.display = 'none';
+    }
+  }
+
+  arrowRight.addEventListener('click', translateRight);
+  arrowLeft.addEventListener('click', translateLeft);
 }
+
+
