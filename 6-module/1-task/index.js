57278ee08a1fcb896bd5bb5eb30970a6404516c7
denyellow <denyellow@gmail.com>
2020-08-31 11:45:00 +0300

    Таблица с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -24,11 +24,50 @@
  *          age: 25,
  *          salary: '1000',
  *          city: 'Petrozavodsk'
- *      },ы
+ *      }
  *
  * @constructor
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+
+    let head = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+    `
+    this.elem.insertAdjacentHTML(`afterbegin`, head);
+
+    for (let row of rows) {
+      let template = `
+        <tbody>
+          <tr>
+            <td>${row.name}</td>
+            <td>${row.age}</td>
+            <td>${row.salary}</td>
+            <td>${row.city}</td>
+            <td><button>X</button></td>
+          </tr>
+        </tbody>
+      `
+      this.elem.insertAdjacentHTML(`afterbegin`, template);
+    }
+
+    let buttons = this.elem.querySelectorAll('button');
+    for (let button of buttons) {
+      button.onclick = function () {
+        button.parentElement.parentElement.remove();
+      };
+    }
   }
-}
+};
+
+
+
