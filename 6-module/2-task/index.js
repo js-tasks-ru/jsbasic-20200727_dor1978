876beeba57cc1b001fdde5c631e810d41ab1fa9e
denyellow <denyellow@gmail.com>
2020-09-02 14:42:27 +0300

    Учебный проект: Карточка товара

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -2,5 +2,36 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class ProductCard {
   constructor(product) {
+    this.elem = document.createElement('div');
+    this.elem.className = "card";
+    this.product = product;
+
+    let template = `
+      <div class="card">
+        <div class="card__top">
+            <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+            <span class="card__price">€${product.price.toFixed(2)}</span>
+        </div>
+        <div class="card__body">
+            <div class="card__title">${product.name}</div>
+            <button type="button" class="card__button">
+                <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+        </div>
+      </div>
+    `
+
+    this.elem.insertAdjacentHTML(`afterbegin`, template);
+
+    let button = this.elem.querySelector('.card__button');
+
+    button.addEventListener('click', () => {
+      button.dispatchEvent(new CustomEvent('product-add', {
+        detail: this.product.id,
+        bubbles: true
+      }));
+    });
   }
 }
+
+
